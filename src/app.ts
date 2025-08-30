7ce75915393d351ef7813c9cca5e03a4721d6a63
saulocapistrano <saulocapistrano@gmail.com>
2025-08-30 10:52:21 -0300

    feat: integrate router for API endpoints in app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import router from './routes';
 
 dotenv.config();
 
@@ -8,5 +9,6 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use('/api', router);
 
 export default app;

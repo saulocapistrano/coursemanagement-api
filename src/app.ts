e3c8c749209f9e60aae55702312baa9573fb39ee
saulocapistrano <saulocapistrano@gmail.com>
2025-08-31 15:00:57 -0300

    feat: integrate Swagger setup in app.ts for API documentation

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import router from './routes';
+import { setupSwagger } from './swagger';
 
 dotenv.config();
 
@@ -10,5 +11,6 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use('/api', router);
+setupSwagger(app);
 
 export default app;

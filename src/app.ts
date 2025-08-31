5897ff2cce5bfd1452fe9cb4324283fe18214794
saulocapistrano <saulocapistrano@gmail.com>
2025-08-31 15:20:12 -0300

    feat: add global error handling middleware to enhance application stability

--- src/app.ts
+++ src/app.ts
@@ -3,14 +3,22 @@ import cors from 'cors';
 import dotenv from 'dotenv';
 import router from './routes';
 import { setupSwagger } from './swagger';
+import { errorHandler } from '@middlewares/errorHandler';
 
 dotenv.config();
 
 const app = express();
 
+// Middlewares globais
 app.use(cors());
 app.use(express.json());
+
+// Routes
 app.use('/api', router);
+
+// Swagger
 setupSwagger(app);
 
+// Middleware de erro
+app.use(errorHandler);
 export default app;
